6ddfe09d9e193a39c5bb638e8d08d919ea1b7369
Ben Clarkson <ben.a.clarkson@gmail.com>
2020-09-07 11:48:01 -0400

    Initial commit of Debugging exercises.

---
+++ Debugging/animal_kingdom.rb
@@ -0,0 +1,58 @@
+# Animal Kingdom
+
+# The code below raises an exception. Examine the error message and alter the code so that it runs without error.
+
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+# The error occurs on `line 42`. Invoking `super` without any parentheses passes all arguments to SuperClass#initialize. When calling only a partial number of parameters with the `super` keyword, those parameters must be specified explicitly.
+
+# Further Exploration: The `FlightlessBird#initialize` method is not necessary because that exact method is already inherited from `Bird` class, which inherits from the `Animal` class.
